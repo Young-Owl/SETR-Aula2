835278763a9c8809f428b69352a1f15acbb395c7
Young-Owl <goncalorodrigues@ua.pt>
2023-02-28 10:56:29 +0000

    Merged all files together

--- main.c
+++ main.c
@@ -1,2 +1,31 @@
 // Main File for the project
-// zz
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+struct dailytemp {
+    int hour;
+    int temp[24];
+    int max;
+    int min;
+    int avg;
+};
+
+void dtInit(struct dailytemp *dt) {
+    dt->hour = 0;
+    for(int i = 0; i<24; i++){
+        dt->temp[i] = 0;
+    }
+    dt->max = 0;
+    dt->min = 0;
+    dt->avg = 0;
+}
+
+void dtAdd(struct dailytemp *dt, int tmp){ 
+    if(tmp > -50 && tmp < 50){
+        dt->temp[dt->hour]=tmp;
+    }
+}
+
+void dtState(struct dailytemp *dt);
